a4065d3b84d0057abe58727706d230bce7b5a205
reymark196 <reymarkgarcia196@gmail.com>
2026-02-13 18:39:23 +0200

    update

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -125,11 +125,6 @@ export function Navbar() {
 												LinkedIn
 											</Link>
 										</DropdownMenuItem>
-										<DropdownMenuItem asChild>
-											<Link href={siteConfig.links.twitter} target="_blank" rel="noreferrer">
-												Twitter
-											</Link>
-										</DropdownMenuItem>
 									</DropdownMenuContent>
 								</DropdownMenu>
 							</div>
